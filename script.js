aeb877a11e6faacdb56d3defd8eafc9ccc08ae9d
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-08-01 15:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,35 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
-
+function fetchData(apiUrl)
+	{
+		const startTime=Date.now();
+		return fetch(apiUrl)
+		.then(()=>Date.now-startTime)
+		.catch(()=>Date.now-startTime)
+	}
+function displayData(outputId,time){
+	const outputCell=document.getElementById(outputId);
+	outputCell.textContent=time;
+}
+function fetchDataUsingAll(){
+	const allPromises=apiUrls.map(fetchData);
+	Promise.all(allPromises)
+	.then((results)=>{
+		const totalTime=results.reduce((total,time)=>total+time,0);
+		displayData('output-all',totalTime);
+	})
+	.catch((error)=>
+		{
+			console.error('Error':error);
+		})
+}
+function fetchDataUsingAny(){
+	const anyPromises=apiUrls.map(fetchData);
+	Promise.any(anyPromises)
+	.then((time)=>displayData('output-any',time));
+	.catch((error)=>console.error('Error',error));
+}
+fetchDataUsingAll();
+fetchDataUsingAny();
 // You can write your code here
